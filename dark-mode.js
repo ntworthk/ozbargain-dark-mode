0ff0de4298b217d7029c636dd8e1f4adbe7ea0bb
nick <nick.twort@houstonkemp.com>
2019-05-14 14:25:32 +1000

    Fix node change and consolidate into one final.

--- dark-mode.js
+++ dark-mode.js
@@ -20,3 +20,19 @@ var links = document.getElementsByTagName("a");
 		link.style.color = "red";
 	}
 });
+
+function changeClass(object, oldClass, newClass) {
+	var regExp = new RegExp('(?:^|\\s)' + oldClass + '(?!\\S)', 'g');
+	object.className = object.className.replace(regExp, newClass);
+}
+function addClass(object, newClass) {
+	object.className += " " + newClass;
+}
+
+
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = 'div.node-teaser-new {  border-bottom: #ddd solid 1px; margin-bottom: 0; padding: 10px 0;} div.node-teaser-new:hover { background-color: #000000;}';
+document.getElementsByTagName('head')[0].appendChild(style);
+var nodies = document.getElementsByClassName('node-teaser');
+for (var i = 0; i < nodies.length; i++) { addClass(nodies[i], 'node-teaser-new')};
