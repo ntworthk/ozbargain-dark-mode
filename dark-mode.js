ed9fca2819c8c9b50c463e319d119a6bbeec0e24
nick <nick.twort@houstonkemp.com>
2019-05-15 17:11:53 +1000

    Fix fix at right panel

--- dark-mode.js
+++ dark-mode.js
@@ -34,5 +34,43 @@ var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = 'div.node-teaser-new {  border-bottom: #ddd solid 1px; margin-bottom: 0; padding: 10px 0;} div.node-teaser-new:hover { background-color: #000000;}';
 document.getElementsByTagName('head')[0].appendChild(style);
+
 var nodies = document.getElementsByClassName('node-teaser');
-for (var i = 0; i < nodies.length; i++) { addClass(nodies[i], 'node-teaser-new')};
+for (let node of nodies) {
+	addClass(node, 'node-teaser-new')
+};
+
+var coupons = document.getElementsByClassName("couponcode");
+for (let coupon of coupons) {
+	coupon.style.color = 'black';
+}
+
+var newsletters = document.getElementsByClassName('newsletter');
+for (let news of newsletters) {
+	news.style.backgroundColor = 'black';
+}
+
+var blocks = document.getElementsByClassName('block');
+for (let block of blocks) {
+	block.style.backgroundColor = '#323639';
+}
+
+var blockTitles = document.getElementsByClassName('blocktitle');
+for (let title of blockTitles) {
+	title.style.color = 'black';
+	title.style.backgroundColor = '#c1c1c1';
+}
+
+
+/*
+// Attempting to fix hover behaviour - it's actually in the li element
+var style2 = document.createElement('style');
+style2.type = 'text/css';
+style2.innerHTML = 'div.new-new {  border-right-color: #828282;} div.new-new:hover { background-color: #000000;}';
+document.getElementsByTagName('head')[0].appendChild(style2);
+
+var newDeals = document.getElementsByClassName('new');
+for (let deal of newDeals) {
+	addClass(deal, 'new-new')
+}
+*/
